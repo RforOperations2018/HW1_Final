ff6df6d038d0858f86ae240a09a58f39b80bcb28
Dominic <dcontrer@andrew.cmu.edu>
2018-09-07 19:13:28 -0400

    Commit 4
    Render plots + data table

--- HW_1_Final/app.R
+++ HW_1_Final/app.R
@@ -75,4 +75,47 @@ server <- function(input, output) {
       melt(id = "model")
   })
   
+  output$plot <- renderPlotly({
+    dat <- carsInput()  # loads data based on input parameters (model + slider range)
+    ggplotly(  # function that creates interactive visualizations for each data point
+      ggplot(data = dat, aes(x = reorder(model, hp), y = hp, fill = hp,  # sets ggplot aesthetics
+                             text = paste0("<b>", model, ":</b> ",  # information parameters for pop-up boxes
+                                           "<br>MPG: ", mpg,
+                                           "<br>Weight (lbs): ", format(wt, big.mark=","),
+                                           "<br>Cylinders: ", cyl,
+                                           "<br>Horsepower: ", hp))) + 
+        geom_bar(stat = "Identity") +  # creates bar chart
+        scale_fill_gradient(low = "red", high = "green") +  # color scale for bar fill
+        labs(x = "Car Model",  # axis and main title labels
+             y = "Horsepower",
+             title = "Horsepower by Car Model") +
+        theme(plot.title = element_text(family = 'Helvetica',  # title formatting
+                                        color = '#181414', 
+                                        face = 'bold', 
+                                        size = 18, 
+                                        hjust = 0)) +
+        theme(axis.title.x = element_text(family = 'Helvetica',  # x-axis formatting
+                                          color = '#181414', 
+                                          face = 'bold', 
+                                          size = 12, 
+                                          hjust = 0)) +
+        theme(axis.title.y = element_text(family = 'Helvetica',  # y-axis formatting
+                                          color = '#181414', 
+                                          face = 'bold', 
+                                          size = 12, 
+                                          hjust = 0)) +
+        theme(legend.position = "none") +  # deletes legend
+        theme(axis.text.x = element_text(angle = 60, vjust = 1, hjust = 1)) +  # tilts x-axis labels
+        guides(color = FALSE)
+      , tooltip = "text")
+  })
+  output$table <- DT::renderDataTable({  # renders data table
+    cars <- carsInput()
+    
+    subset(cars, select = c(model, mpg, cyl, hp, wt))  # select features to include in data table
+  })
+  observe({
+    print(reactiveValuesToList(input))
+  })
+}
 
